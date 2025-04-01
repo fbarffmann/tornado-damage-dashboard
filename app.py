085a71cdc00d39b3cf4513dc2c8fb91235a41020
fbarffmann <finnba894@gmail.com>
2025-03-31 22:54:09 -0500

    Final Flask update for Render compatibility

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
@@ -68,4 +69,5 @@ def get_data():
 
 # -----------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
